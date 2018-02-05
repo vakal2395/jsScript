4fe35c2d6ab3567fac3af1398a9cac1c2af54548
vakal2395 <vakal707823@gmail.com>
2018-02-05 13:21:37 +0300

    add

--- dzScript/script.js
+++ dzScript/script.js
@@ -1,4 +1,43 @@
-var firstName;
-firstName = prompt("your name");
-var sum = "Hуllo " + firstName;
-document.write(sum);
+/*var a=0;
+var b=0;
+while(a <= 100){
+b+=a;
+a++;
+console.log(b);
+}*/
+
+
+var i = 0;
+
+while (i <= 100) {
+  i++;
+  if (i%3 == 0) {
+   document.write(i + ' ');
+  }
+  
+}
+	
+
+/*for (var i = 0; i <=  100; i++) {
+
+	if(i % 3===0){
+
+	document.write(i + ' ');
+
+	}
+	
+}
+/*var b = 8;
+var c = 3;
+
+<!--if (a < c) {
+document.write("a > b");
+}else if(a<b){
+document.write("a < b");
+} else if(a>b){
+document.write("a > c");
+}
+
+else{
+	document.write("error");
+}*/
