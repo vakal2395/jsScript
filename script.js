8a04b5eb24ebae6eb5b4be75d52edb04820b37fb
vakal2395 <vakal707823@gmail.com>
2018-02-09 11:25:40 +0300

    factorial

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
-var a=0;
-var b=0;
-while(a <= 100){
-b+=a;
-a++;
-console.log(b);
+// var a=Number(prompt("input firs number"));
+
+var a=Number(prompt("input firs number"));
+
+var result = 1;
+var i = 1;
+while (i <= a) {
+result = result * i;
+i++;
+
+document.write(result +" ");
 }
 
 
-var i = 0;
-
+// var result = 1;
+// var i = 1;
+// do {
+// result = result * i;
+// i++;
+// }
+// while (i <= a){
+// document.write(result);
+// }
+// var i = 0;
+  
 while (i <= 100) {
   i++;
-  if (i%3 == 0) {
-   document.write(i + ' ');
+  if (i%3 == 0 && i%2==1) {
+   console.log(i + ' ');
   }
   
 }
-	
-
-/*for (var i = 0; i <=  100; i++) {
-
-	if(i % 3===0){
-
-	document.write(i + ' ');
-
-	}
-	
-}*/
-/*var b = 8;
-var c = 3;
-
-<!--if (a < c) {
-document.write("a > b");
-}else if(a<b){
-document.write("a < b");
-} else if(a>b){
-document.write("a > c");
-}
-
-else{
-	document.write("error");
-}*/
