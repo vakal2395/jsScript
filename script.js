ee5633936727f90442b69bc27eb7db03aef3e3da
vakal2395 <vakal707823@gmail.com>
2018-02-16 13:17:57 +0300

    add

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // var a=Number(prompt("input firs number"));
 
-var a=Number(prompt("input firs number"));
+var a = prompt("В шкафу лежит двадцать два синих носка и тридцать пять черных носков. Вам надо в полной темноте взять из шкафа пару носков. Сколько носков нужно взять, чтобы с гарантией получить совпадающую пару ?  (ответ словом)!!!");
 
-var result = 1;
-var i = 1;
-while (i <= a) {
-result = result * i;
-i++;
-
-document.write(result +" ");
+if(a == "три"){
+	alert("Ты молодец! Oтвет правельный, можешь с уверенностью покутать себе носки");
+}else if(a == "3"){
+	alert("Ты молодец! Oтвет правельный, можешь с уверенностью покутать себе носки");
 }
+else{
+	alert("неправильно подумай еще");
+}
+
+var str = "Я-javascript!";
+	document.write(str);
+// var result = 1;
+// var i = 1;
+// while (i <= a) {
+// result = result * i;
+// i++;
+
+// document.write(result +" ");
+// }
 
 
 // var result = 1;
@@ -23,10 +34,11 @@ document.write(result +" ");
 // }
 // var i = 0;
   
-while (i <= 100) {
-  i++;
-  if (i%3 == 0 && i%2==1) {
-   console.log(i + ' ');
-  }
+// while (i <= 100) {
+//   i++;
+//   if (i%3 == 0 && i%2==1) {
+	
+//    console.log(i + ' ');
+//   }
   
-}
+// }
